b35e2f5f6f9898e1e453b81f7f337c6df4b8a3be
Jaisson Santos <jaissonssantos@gmail.com>
2018-02-14 15:35:03 -0500

    Finalização do placar eleitoral;

--- controller/apuracao/2014_1turnoDeputadoFederal.php
+++ controller/apuracao/2014_1turnoDeputadoFederal.php
@@ -19,7 +19,7 @@ try {
 		FROM eleicoes2014apuracao		
 		WHERE cargo="DEPUTADO FEDERAL"
 		AND turno=1 
-        AND numerovotavel NOT IN(95,96)
+        AND numerovotavel NOT IN(10,11,12,13,14,15,17,18,19,20,22,23,25,27,31,33,35,36,40,43,44,45,50,55,51,65,77,90,95,96)
 		'. $cidade .' 
 		GROUP BY nomevotavel
 		ORDER BY votos DESC'
